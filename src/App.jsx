f6f5674a1f79b9a8525da9eb371d648a41aba38f
joemiddie13 <joemiddie13@gmail.com>
2025-03-27 14:04:41 -0700

    Lift the state to the App component to allow sharing data between counters

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import Counter from './Counter';
 
 function App() {
+  const [counts, setCounts] = useState([1, 4, 3]);
+
+  const updateCount = (index, delta) => {
+    const newCounts = [...counts];
+    newCounts[index] += delta;
+    setCounts(newCounts);
+  };
+
   return (
     <div className="App">
-      <Counter label="Apples" value={1} />
-      <Counter label="Oranges" value={3} />
+      {counts.map((value, index) => (
+        <Counter
+          key={index}
+          label={`Counter ${index + 1}`}
+          value={value}
+          increment={() => updateCount(index, 1)}
+          decrement={() => updateCount(index, -1)}
+        />
+      ))}
     </div>
   );
 }
